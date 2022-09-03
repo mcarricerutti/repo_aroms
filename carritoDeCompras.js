48f8cfecfe3f57f6051de1271700f213a6d65486
mcarricerutti <martinacarri345@gmail.com>
2022-09-03 15:11:44 -0300

    Agregando Fetch

--- carritoDeCompras.js
+++ carritoDeCompras.js
@@ -1,29 +1,51 @@
-let ingreso=(
-    Swal.fire({
-        position: 'center',
-        text: `Ingresa tu nombre`,
-        input: `text`,
-        inputPlaceHolder: `Escribe aquí...`,
-        confirmButtonColor: `pink`,
-        confirmButtonText: `Listo`
-})
-)
+// let ingreso=(
+//     Swal.fire({
+//         position: 'center',
+//         text: `Ingresa tu nombre`,
+//         input: `text`,
+//         inputPlaceHolder: `Escribe aquí...`,
+//         confirmButtonColor: `pink`,
+//         confirmButtonText: `Listo`
+// })
+// )
 
-for(let i=0;i==1;i++){
-    ingreso;
+// for(let i=0;i==1;i++){
+//     ingreso;
+// }
+
+
+// //Almacenamos productos en el localStorage
+// const guardarUsuario = (clave, valor) => {
+// localStorage.setItem(clave, valor)
+// };
+// //Guardamos el array completo
+// guardarUsuario("Usuarios",ingreso);
+
+
+
+//Creamos una variable global para almacenas los datos de productos.json
+let productosJSON = [];
+
+//Traemos los datos de productos.json
+async function obtenerJSON() {
+    const URLJSON="productos.json"
+    //Traemos toda la info
+    const resp=await fetch(URLJSON)
+    //Guardamos la info ya pasada a objeto
+    const data= await resp.json()
+    //Y a esa info la guardamos en una variable global
+    productosJSON = data;
+
+    //Presentamos los productos
+    presentarProductos();
 }
 
-localStorage.setItem("usuario",ingreso);
+//Llamamos la función
+obtenerJSON();
 
 //Declaramos array carrito,para utilizarlo luego
-let Carrito = [];
+let Carrito =[];
 
-//Almacenamos productos en el localStorage
-const guardarLocal = (clave, valor) => {
-    localStorage.setItem(clave, valor)
-};
-//Guardamos el array completo
-guardarLocal("listaProductos", JSON.stringify(presentarProductos));
 
 
 //Llamamos elementos del html(en este caso todos son id)
@@ -35,77 +57,70 @@ const DOMbotonVaciar = document.querySelector('#boton-vaciar');
 
 //Funciones
 
-
+//Dibujamos todos los productos
 function presentarProductos() {
-    const URLGET = "users.json";
-    fetch(URLGET)
-        .then(resultado => resultado.json())
-        .then(velas => {
-            velas.Productos.forEach((info) => {
-                // Estructura
-                const miNodo = document.createElement('div');
-                miNodo.classList.add('card', 'col-sm-4');
-                // Body
-                const miNodoCardBody = document.createElement('div');
-                miNodoCardBody.classList.add('card-body');
-                // Titulo
-                const miNodoTitle = document.createElement('h5');
-                miNodoTitle.classList.add('card-title');
-                miNodoTitle.style.color = `#7C86C5`;
-                miNodoTitle.style.fontSize = `22px`;
-                miNodoTitle.style.padding = `5%`;
-                miNodoTitle.style.fontWeight = `bold`;
-                miNodoTitle.textContent = info.aroma;
-                // Imagen
-                const miNodoImagen = document.createElement('img');
-                miNodoImagen.classList.add('img-fluid');
-                miNodoImagen.setAttribute('src', info.imagen);
-                // Precio
-                const miNodoPrecio = document.createElement('p');
-                miNodoPrecio.classList.add('card-text');
-                miNodoPrecio.style.color = `#7A7D8F `;
-                miNodoPrecio.style.backgroundColor = `#D3D7EC`;
-                miNodoPrecio.style.marginLeft = `10%`;
-                miNodoPrecio.style.marginRight = `20%`;
-                miNodoPrecio.textContent = `$ ${info.precio}`;
-                //Tamano
-                const miNodoTamano = document.createElement(`tamano`);
-                miNodoTamano.classList.add(`card-text`);
-                miNodoTamano.style.fontSize = `18px`;
-                miNodoTamano.textContent = `${info.tamano}`;
-                // Boton 
-                const miNodoBoton = document.createElement('button');
-                miNodoBoton.style.marginLeft = `10%`;
-                miNodoBoton.style.backgroundColor = `#A7ADD1`;
-                miNodoBoton.style.border = `none`;
-                miNodoBoton.classList.add('btn');
-                miNodoBoton.textContent = '+';
-                miNodoBoton.setAttribute('marcador', info.id);
-                miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
+productosJSON.forEach((info) => {
+    // Estructura
+    const miNodo = document.createElement('div');
+    miNodo.classList.add('card', 'col-sm-4');
+    // Body
+    const miNodoCardBody = document.createElement('div');
+    miNodoCardBody.classList.add('card-body');
+    // Titulo
+    const miNodoTitle = document.createElement('h5');
+    miNodoTitle.classList.add('card-title');
+    miNodoTitle.style.color = `#7C86C5`;
+    miNodoTitle.style.fontSize = `22px`;
+    miNodoTitle.style.padding = `5%`;
+    miNodoTitle.style.fontWeight = `bold`;
+    miNodoTitle.textContent = info.aroma;
+    // Imagen
+    const miNodoImagen = document.createElement('img');
+    miNodoImagen.classList.add('img-fluid');
+    miNodoImagen.setAttribute('src', info.imagen);
+    // Precio
+    const miNodoPrecio = document.createElement('p');
+    miNodoPrecio.classList.add('card-text');
+    miNodoPrecio.style.color = `#7A7D8F `;
+    miNodoPrecio.style.backgroundColor = `#D3D7EC`;
+    miNodoPrecio.style.marginLeft = `10%`;
+    miNodoPrecio.style.marginRight = `20%`;
+    miNodoPrecio.textContent = `$ ${info.precio}`;
+    //Tamano
+    const miNodoTamano = document.createElement(`tamano`);
+    miNodoTamano.classList.add(`card-text`);
+    miNodoTamano.style.fontSize = `18px`;
+    miNodoTamano.textContent = `${info.tamano}`;
+    // Boton 
+    const miNodoBoton = document.createElement('button');
+    miNodoBoton.style.marginLeft = `10%`;
+    miNodoBoton.style.backgroundColor = `#A7ADD1`;
+    miNodoBoton.style.border = `none`;
+    miNodoBoton.classList.add('btn');
+    miNodoBoton.textContent = '+';
+    miNodoBoton.setAttribute('marcador', info.id);
+    miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
 
-                //Disponibilidad
-                const miNodoDispo = document.createElement(`disponible`);
-                miNodoDispo.classList.add(`card-text`);
-                miNodoDispo.style.fontSize = `15px`;
-                miNodoDispo.style.display = `flex`;
-                miNodoDispo.style.paddingTop = `4%`;
-                miNodoDispo.textContent = `\n Compra máx. ${info.stockMax}u`;
+    //Disponibilidad
+    const miNodoDispo = document.createElement(`disponible`);
+    miNodoDispo.classList.add(`card-text`);
+    miNodoDispo.style.fontSize = `15px`;
+    miNodoDispo.style.display = `flex`;
+    miNodoDispo.style.paddingTop = `4%`;
+    miNodoDispo.textContent = `\n Compra máx. ${info.stockMax}u`;
 
 
-                // Insertamos todo el codigo creado anteriormente a la etiqueta padre
-                miNodoCardBody.appendChild(miNodoImagen);
-                miNodoCardBody.appendChild(miNodoTitle);
-                miNodoCardBody.appendChild(miNodoPrecio);
-                miNodoCardBody.appendChild(miNodoTamano);
-                miNodoCardBody.appendChild(miNodoBoton);
-                miNodoCardBody.appendChild(miNodoDispo);
-                miNodo.appendChild(miNodoCardBody);
-                DOMitems.appendChild(miNodo);
+    // Insertamos todo el codigo creado anteriormente a la etiqueta padre
+    miNodoCardBody.appendChild(miNodoImagen);
+    miNodoCardBody.appendChild(miNodoTitle);
+    miNodoCardBody.appendChild(miNodoPrecio);
+    miNodoCardBody.appendChild(miNodoTamano);
+    miNodoCardBody.appendChild(miNodoBoton);
+    miNodoCardBody.appendChild(miNodoDispo);
+    miNodo.appendChild(miNodoCardBody);
+    DOMitems.appendChild(miNodo);
 
-            });
-        })
-}
-
+});
 
 
 /**
@@ -128,102 +143,99 @@ function anyadirProductoAlCarrito(evento) {
     // Actualizamos el carrito 
     presentarCarrito();
 }
-
-
-
-
+}
 
 /**
- * Dibujamos todos los productos guardados en el carrito
- */
+* Dibujamos todos los productos guardados en el carrito
+*/
 function presentarCarrito() {
-    // Vaciamos todo el html
-    DOMcarrito.textContent = '';
-    // Quitamos los duplicados
-    const carritoSinDuplicados = [...new Set(Carrito)];
-    // Generamos los Nodos a partir de carrito
-    carritoSinDuplicados.forEach((item) => {
-        // Obtenemos el item que necesitamos de la variable Producto
-        const miItem =Productos.filter((itemProducto) => {
-            // ¿Coincide las id? Solo puede existir un caso
-            return itemProducto.id === parseInt(item);
-        });
-        // Cuenta el número de veces que se repite el producto
-        const numeroUnidadesItem = Carrito.reduce((total, itemId) => {
-            // ¿Coincide las id? Incremento el contador, en caso contrario lo mantengo
-            return itemId === item ? total += 1 : total;
-        }, 0);
-
-
-        // Creamos el nodo del item del carrito
-        const miNodo = document.createElement('li');
-        miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
-        miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].aroma} - $ ${miItem[0].precio}`;
-        // Boton de borrar
-        const miBoton = document.createElement('button');
-        miBoton.classList.add(`btn`, 'mx-5');
-        miBoton.textContent = '🗑';
-        miBoton.style.backgroundColor = `#B7757B `;
-        miBoton.style.border = `#E82216`
-        miBoton.style.marginLeft = '1rem';
-        miBoton.dataset.item = item;
-        miBoton.addEventListener('click', borrarItemCarrito);
-
-        // Mezclamos nodos
-        miNodo.appendChild(miBoton);
-        DOMcarrito.appendChild(miNodo);
+// Vaciamos todo el html
+DOMcarrito.textContent = '';
+// Quitamos los duplicados
+const carritoSinDuplicados = [...new Set(Carrito)];
+// Generamos los Nodos a partir de carrito
+carritoSinDuplicados.forEach((item) => {
+    // Obtenemos el item que necesitamos de la variable Producto
+    const miItem = productosJSON.filter((itemProducto) => {
+        // ¿Coincide las id? Solo puede existir un caso
+        return itemProducto.id === parseInt(item);
     });
-    // Renderizamos el precio total en el HTML
-    DOMtotal.textContent = `$` + calcularTotal();
+    // Cuenta el número de veces que se repite el producto
+    const numeroUnidadesItem = Carrito.reduce((total, itemId) => {
+        // ¿Coincide las id? Incremento el contador, en caso contrario lo mantengo
+        return itemId === item ? total += 1 : total;
+    }, 0);
+
+
+    // Creamos el nodo del item del carrito
+    const miNodo = document.createElement('li');
+    miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+    miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].aroma} - $ ${miItem[0].precio}`;
+    // Boton de borrar
+    const miBoton = document.createElement('button');
+    miBoton.classList.add(`btn`, 'mx-5');
+    miBoton.textContent = '🗑';
+    miBoton.style.backgroundColor = `#B7757B `;
+    miBoton.style.border = `#E82216`
+    miBoton.style.marginLeft = '1rem';
+    miBoton.dataset.item = item;
+    miBoton.addEventListener('click', borrarItemCarrito);
+
+    // Mezclamos nodos
+    miNodo.appendChild(miBoton);
+    DOMcarrito.appendChild(miNodo);
+});
+// Renderizamos el precio total en el HTML
+DOMtotal.textContent = `$` + calcularTotal();
 }
 
 
 /**
- * Evento para borrar un elemento del carrito
- */
+* Evento para borrar un elemento del carrito
+*/
 function borrarItemCarrito(evento) {
-    // Obtenemos el producto ID que hay en el boton pulsado
-    const id = evento.target.dataset.item;
-    // Borramos todos los productos
-    Carrito = Carrito.filter((carritoId) => {
-        return carritoId !== id;
-    });
+// Obtenemos el producto ID que hay en el boton pulsado
+const id = evento.target.dataset.item;
+// Borramos todos los productos
+Carrito = Carrito.filter((carritoId) => {
+    return carritoId !== id;
+});
 
-    Swal.fire({
-        text: 'Deseas eliminar el producto?',
-        icon: 'warning',
-        showCancelButton: false,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Si!'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            Swal.fire(
-                'Eliminado!',
-                'Su producto ha sido eliminado.',
-                'success'
-            )
-        }
-    })
+Swal.fire({
+    text: 'Deseas eliminar el producto?',
+    icon: 'warning',
+    showCancelButton: false,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Si!'
+}).then((result) => {
+    if (result.isConfirmed) {
+        Swal.fire(
+            'Eliminado!',
+            'Su producto ha sido eliminado.',
+            'success'
+        )
+    }
+})
 
-    // volvemos a renderizar
-    presentarCarrito();
+// volvemos a renderizar
+presentarCarrito();
 }
 
 
 /**
- * Calcula el precio total teniendo en cuenta los productos repetidos
- */
+* Calcula el precio total teniendo en cuenta los productos repetidos
+*/
 function calcularTotal() {
-    // Recorremos el array del carrito 
-    return Carrito.reduce((total, item) => {
-        // De cada elemento obtenemos su precio
-        const miItem = Productos.filter((itemProducto) => {
-            return itemProducto.id === parseInt(item);
-        });
-        // Los sumamos al total
-        return total + miItem[0].precio;
-    }, 0).toFixed(2);
+// Recorremos el array del carrito 
+return Carrito.reduce((total, item) => {
+    // De cada elemento obtenemos su precio
+    const miItem = productosJSON.filter((itemProducto) => {
+        return itemProducto.id === parseInt(item);
+    });
+    // Los sumamos al total
+    return total + miItem[0].precio;
+}, 0).toFixed(2);
 }
 
 
@@ -233,28 +245,28 @@ let miBotonF = document.getElementById("boton-Fin");
 miBotonF.addEventListener("click", ejecutar);
 
 function ejecutar() {
-    Swal.fire({
-        title: `Compra realizada con éxito!`,
-        color: `white`,
-        background: `#778c9e`,
-        confirmButtonColor: `grey`,
-        confirmButtonText: `✓`,
-        icon: 'success',
-    })
-    // }).then(() => {
-    //     calcularTotal()>5000 ? Swal.fire({text:"En su compra tendrá un descuento"}): Swal.fire({text:"No tendrá descuento",})//Aplicando operador ternario
-    //   })
+Swal.fire({
+    title: `Compra realizada con éxito!`,
+    color: `white`,
+    background: `#778c9e`,
+    confirmButtonColor: `grey`,
+    confirmButtonText: `✓`,
+    icon: 'success',
+})
+// }).then(() => {
+//     calcularTotal()>5000 ? Swal.fire({text:"En su compra tendrá un descuento"}): Swal.fire({text:"No tendrá descuento",})//Aplicando operador ternario
+//   })
 }
 
 
 /**
- * Vacía el carrito y vuelve a dibujarlo
- */
+* Vacía el carrito y vuelve a dibujarlo
+*/
 function vaciarCarrito() {
-    // Limpiamos los productos guardados
-    Carrito = [];
-    // Renderizamos los cambios
-    presentarCarrito();
+// Limpiamos los productos guardados
+Carrito = [];
+// Renderizamos los cambios
+presentarCarrito();
 }
 // Eventos
 DOMbotonVaciar.addEventListener('click', vaciarCarrito);
@@ -263,10 +275,3 @@ DOMbotonVaciar.addEventListener('click', vaciarCarrito);
 // Inicio
 presentarProductos();
 presentarCarrito();
-
-
-
-
-
-
-
